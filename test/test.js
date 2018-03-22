dba42a3ff7700d21a33384b68b632eac48c4997a
Ryosuke Otsuya <r.otsuya@gmail.com>
2018-03-22 14:13:17 +0900

    大津谷ターン終わり

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const assert   = require('power-assert');  // power-assertをインポート
 const myModule = require('../index');      // プログラム本体である、index.jsを読み込む
 
-describe('開発環境が動く', () => {
-    it('テストが失敗する', () => {
-        assert(true === false);
+describe('整数の閉区間を示すオブジェクト', () => {
+    describe('整数閉区間を示すオブジェクトを生成できるクラスとコンストラクタを作る', () => {
+        it('引数なしでnewするとプリミティブ型がobjectの何かが返る', () => {
+            assert(typeof  === false);
+        });
     });
 });
